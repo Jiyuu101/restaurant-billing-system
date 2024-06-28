02ea9712d0bafaf68764dddd1d65020f22147f6a
Jiyuu101 <mkyaw6240@gmail.com>
2024-06-28 13:27:27 +0630

    bean

--- src/main/java/restaurant_billing_system/controllers/OrderController.java
+++ src/main/java/restaurant_billing_system/controllers/OrderController.java
@@ -1,5 +1,14 @@
 package restaurant_billing_system.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import restaurant_billing_system.persistance.OrderRepository;
+
+@Controller
 public class OrderController {
+@Autowired
+OrderRepository orderRepo;
+
 
 }
--- src/main/java/restaurant_billing_system/persistance/MenuItemTypeRepository.java
+++ src/main/java/restaurant_billing_system/persistance/MenuItemTypeRepository.java
@@ -1,5 +1,49 @@
 package restaurant_billing_system.persistance;
 
-public class MenuItemTypeRepository {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import restaurant_billing_system.models.MenuItemType;
+import restaurant_billing_system.models.Order;
+
+public class MenuItemTypeRepository {
+	// connection
+	public static Connection con = null;
+	static {
+		con = MyConnection.getConnection();
+	}
+	
+	public int add(MenuItemType menuItemType) {
+		String sql="INSERT INTO menu_item_type(id,type)VALUES(?,?)";
+		int result;
+		try {
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setInt(1, menuItemType.getId());
+			ps.setString(2, menuItemType.getType());
+			
+			result=ps.executeUpdate();
+			
+		}catch(SQLException e) {
+			result=0;
+			System.out.println("Menu item type insert error: "+e);
+	    }
+		return result;
+	}
+	
+	public int edit(MenuItemType menuItemType) {
+		int result=0;
+		String sql="UPDATE order SET type=? WHERE id=?";
+		try {
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setString(1, menuItemType.getType());
+			
+			result=ps.executeUpdate();
+			
+		}catch(SQLException e) {
+			result=0;
+			System.out.println("Menu item type edit error: "+e);
+	    }
+		return result;
+}
 }
